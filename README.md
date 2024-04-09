457a836ad404e2c3e3c826d063db957eba99a6e9
m1 <mbp98k@gmail.com>
2024-04-10 04:28:47 +0800

    add example & update readme

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # mdw
 gin middleware
 
+### note
+
+Before using the middleware, please remember to init `headerName` `secret` & `expire`.
+
+Here is the example:
+
+```go
+    mdw.SetSecret("balabala..")
+	mdw.SetHeaderName("xxx")
+	mdw.SetJWTExpire(20) //20 seconds
+```
+
+Full example can be seen [here](./example/main.go)
---
+++ example/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/scott-x/mdw"
+	"github.com/scott-x/resp"
+	"log"
+)
+
+func main() {
+	mdw.SetSecret("balabala..")
+	mdw.SetHeaderName("xxx")
+	mdw.SetJWTExpire(20) //20 seconds
+	route := gin.Default()
+	route.Use(mdw.AllowCrossOrigin())
+	auth := route.Group("/auth")
+	auth.Use(mdw.AuthRequired())
+
+	auth.GET("/test", func(c *gin.Context) {
+		type Output struct {
+			Name string `json:"name"`
+			Age  int    `json:"age"`
+		}
+		log.Println("uid:", mdw.GetUid(c))
+		o := Output{
+			Name: "Scott",
+			Age:  18,
+		}
+		resp.Success(c, o)
+	})
+
+	route.POST("/login", func(c *gin.Context) {
+		type User struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}
+		var u User
+		if err := c.ShouldBind(&u); err != nil {
+			resp.ErrorBadRequest(c)
+			return
+		}
+		type Output struct {
+			Token string `json:"token"`
+		}
+
+		if u.Username == "" || u.Password == "" {
+			resp.Error(c, 2001, "用户名或密码不能为空")
+			return
+		}
+
+		if u.Username == "scott" && u.Password == "123" {
+			//success
+			token, err := mdw.CreateJWT(1)
+			log.Println(token)
+			if err != nil {
+				log.Println(err)
+				resp.ErrorServerInternal(c)
+				return
+			}
+			output := Output{Token: token}
+			resp.Success(c, output)
+			return
+		}
+
+		resp.Error(c, 2001, "用户名或密码错误")
+
+	})
+
+	route.GET("/test1", func(c *gin.Context) {
+		resp.Success(c, "success")
+	})
+
+	route.GET("/test2", func(c *gin.Context) {
+		resp.ErrorBadRequest(c)
+	})
+
+	route.GET("/test3", func(c *gin.Context) {
+		resp.ErrorNotAuthorized(c)
+	})
+
+	route.GET("/test4", func(c *gin.Context) {
+		resp.ErrorForbidden(c)
+	})
+
+	route.GET("/test5", func(c *gin.Context) {
+		resp.ErrorNotFound(c)
+	})
+
+	route.GET("/test6", func(c *gin.Context) {
+		resp.ErrorServerInternal(c)
+	})
+
+	route.GET("/test7", func(c *gin.Context) {
+		resp.Error(c, 2004, "username, password does not match")
+	})
+
+	err := route.Run(":8989")
+	if err != nil {
+		return
+	}
+}
